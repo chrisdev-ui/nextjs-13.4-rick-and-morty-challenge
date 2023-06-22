ea988d7ac43c916fc5202226c6aad5e1ae6fe151
chrisdev-ui <web.christian.dev@gmail.com>
2023-06-21 20:49:01 -0500

    feat-01: initial configuration for sass, jest and folders structure

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const nextJest = require('next/jest')
+const createJestConfig = nextJest({
+  dir: './'
+})
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/hooks/(.*)$': '<rootDir>/src/hooks/$1'
+  },
+  testEnvironment: 'jest-environment-jsdom'
+}
+module.exports = createJestConfig(customJestConfig)
